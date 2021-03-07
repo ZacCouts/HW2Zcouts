dc2bebeca9ae6eb8ed8f0b8eb0f734989bf4504c
zcouts <zcouts@ltu.edu>
2021-03-07 18:59:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ app.use(BodyParser.json());
 
 app.use('/', Router);
 
-const db = Mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  // we're connected!
-  console.log('Database connection has been open');
-});
-
 (async () => {
   await Mongoose.connect(process.env.Credentials, {
     useNewUrlParser: true,
