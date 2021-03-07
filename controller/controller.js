a9de0db3dc39158232c27f2f2c4d124fe79257c9
zcouts <zcouts@ltu.edu>
2021-03-07 17:50:38 -0500

    Created Router

--- controller/controller.js
+++ controller/controller.js
@@ -20,13 +20,13 @@ const doActionThatMightFailValidation = async (request, response, action) => {
   }
 };
 
-app.get('/products', async (request, response) => {
+const getProd = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     response.json(await Serv.find(request.query).select('-_id -__v'));
   });
-});
+};
 
-app.get('/products/:sku', async (request, response) => {
+const getProdSKU = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     const getResult = await Serv.findOne({ sku: request.params.sku }).select('-_id -__v');
     if (getResult != null) {
@@ -35,30 +35,30 @@ app.get('/products/:sku', async (request, response) => {
       response.sendStatus(404);
     }
   });
-});
+};
 
-app.post('/products', async (request, response) => {
+const postProd = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     await new Serv(request.body).save();
     response.sendStatus(201);
   });
-});
+};
 
-app.delete('/products', async (request, response) => {
+const deleteProd = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     response.sendStatus((await Serv.deleteMany(request.query)).deletedCount > 0 ? 200 : 404);
   });
-});
+};
 
-app.delete('/products/:sku', async (request, response) => {
+const deleteProdSKU = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     response.sendStatus((await Serv.deleteOne({
       sku: request.params.sku,
     })).deletedCount > 0 ? 200 : 404);
   });
-});
+};
 
-app.put('/products/:sku', async (request, response) => {
+const putProdSKU = async (request, response) => {
   const { sku } = request.params;
   const product = request.body;
   product.sku = sku;
@@ -68,9 +68,9 @@ app.put('/products/:sku', async (request, response) => {
     });
     response.sendStatus(200);
   });
-});
+};
 
-app.patch('/products/:sku', async (request, response) => {
+const patchProdSKU = async (request, response) => {
   const { sku } = request.params;
   const product = request.body;
   delete product.sku;
@@ -86,15 +86,15 @@ app.patch('/products/:sku', async (request, response) => {
       response.sendStatus(404);
     }
   });
-});
+};
 
-app.get('/users', async (request, response) => {
+const getUser = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     response.json(await Serv.find(request.query).select('-_id -__v'));
   });
-});
+};
 
-app.get('/users/:ssn', async (request, response) => {
+const getUserSSN = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     const getResult = await Serv.findOne({ ssn: request.params.ssn }).select('-_id -__v');
     if (getResult != null) {
@@ -103,30 +103,30 @@ app.get('/users/:ssn', async (request, response) => {
       response.sendStatus(404);
     }
   });
-});
+};
 
-app.post('/users', async (request, response) => {
+const postUser = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     await new Serv(request.body).save();
     response.sendStatus(201);
   });
-});
+};
 
-app.delete('/users', async (request, response) => {
+const deleteUser = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     response.sendStatus((await Serv.deleteMany(request.query)).deletedCount > 0 ? 200 : 404);
   });
-});
+};
 
-app.delete('/users/:ssn', async (request, response) => {
+const deleteUserSSN = async (request, response) => {
   await doActionThatMightFailValidation(request, response, async () => {
     response.sendStatus((await Serv.deleteOne({
       ssn: request.params.ssn,
     })).deletedCount > 0 ? 200 : 404);
   });
-});
+};
 
-app.put('/users/:ssn', async (request, response) => {
+const putUserSSN = async (request, response) => {
   const { ssn } = request.params;
   const user = request.body;
   user.ssn = ssn;
@@ -137,9 +137,9 @@ app.put('/users/:ssn', async (request, response) => {
     });
     response.sendStatus(200);
   });
-});
+};
 
-app.patch('/users/:ssn', async (request, response) => {
+const patchUserSSN = async (request, response) => {
   const { ssn } = request.params;
   const user = request.body;
   delete user.ssn;
@@ -155,4 +155,21 @@ app.patch('/users/:ssn', async (request, response) => {
       response.sendStatus(404);
     }
   });
-});
+};
+
+module.exports = {
+  getProd,
+  getProdSKU,
+  postProd,
+  deleteProd,
+  deleteProdSKU,
+  putProdSKU,
+  patchProdSKU,
+  getUser,
+  getUserSSN,
+  postUser,
+  deleteUser,
+  deleteUserSSN,
+  putUserSSN,
+  patchUserSSN,
+};
--- router/router.js
+++ router/router.js
@@ -4,6 +4,18 @@ const router = Express.Router();
 
 const Controller = require('../controller/controller');
 
-router.get('/', Controller.doActionThatMightFailValidation);
+router.get('/', Controller.getProd);
+router.get('/', Controller.getProdSKU);
+router.post('/', Controller.postProd);
+router.delete('/', Controller.deleteProd);
+router.delete('/', Controller.deleteProdSKU);
+router.patch('/', Controller.patchProdSKU);
+
+router.get('/', Controller.getUser);
+router.get('/', Controller.getUserSSN);
+router.post('/', Controller.postUser);
+router.delete('/', Controller.deleteUser);
+router.delete('/', Controller.deleteUserSSN);
+router.patch('/', Controller.patchUserSSN);
 
 module.exports = router;
